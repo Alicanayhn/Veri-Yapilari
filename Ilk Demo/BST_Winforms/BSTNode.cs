abf95b68fd200a092a78a23622c971efe27b899e
Ali Can Ayhan <alicanayhn@gmail.com>
2025-05-12 22:23:17 +0300

    İlk Demo Yapıldı Form Uygulaması Test Edildi

---
+++ Ilk Demo/BST_Winforms/BSTNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Winforms
+{
+    public class BSTNode
+    {
+        public int Key;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int key)
+        {
+            this.Key = key;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+}
